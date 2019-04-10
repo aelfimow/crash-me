dba342ea46d8df4c68c608060c3ce05172e42506
Alexander Elfimow <my.free.mail@gmx.de>
2019-04-10 22:35:55 +0200

    Added crash

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <cstdlib>
 
+#include "div_arithm.h"
+
+
 int main(int argc, char *argv[])
 {
+    div_param dp;
+
+    dp.with_Z1(2).with_Z2(0).with_N(2);
+    div_arithm(&dp);
+
+    dp.with_Z1(2).with_Z2(0x8000000000000000).with_N(2);
+    div_arithm(&dp);
+
     return EXIT_SUCCESS;
 }
